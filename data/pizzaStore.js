582ee0930a571d0a9260b2835a74e2bb44056197
claudio Abud <clau@gmail.com>
2021-09-01 23:09:25 -0500

    uncommented code to write to a posgres database table

--- data/pizzaStore.js
+++ data/pizzaStore.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const database = 'pizza_luvrs'
-const host = '<enter_host_here>'
+const host = 'pizza-db.ctlazj5okmml.us-west-2.rds.amazonaws.com'
 const username = 'postgres'
 const password = 'password'
 
